fd1d0ad4ba9c735d93e2ee301078181eee248eb1
jenny-moore <jenny.moore@mail.utoronto.ca>
2023-09-18 16:54:40 -0400

    Draft of first 3 methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,22 @@
 public class Tree {
+    private int root;
+    private int[] subtrees;
+
+    public Tree(int r, int[] subs) {
+        this.root = r;
+        this.subtrees = subs;
+    }
+
+    boolean is_empty() {
+        return this.root == 0;
+    }
+
+    int length(int[] subtrees) {
+        if (this.is_empty()) {
+            return 0;
+        }
+        else {
+           return length(this.subtrees);
+        }
+    }
 }
